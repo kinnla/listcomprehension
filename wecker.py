a1b4d67cd450f47474e4810fc5f8de4beebd2242
Till Zoppke <till.zoppke@fu-berlin.de>
2017-02-26 14:13:42 +0100

    refactoring: list comprehension and filtering instead of recursion

--- wecker.py
+++ wecker.py
@@ -4,24 +4,10 @@ Computes possible equations for the wecker problem
 """
 
 # start time as 'hh:mm'
-START_TIME = '21:00'
+START_TIME = '20:12'
 
 # time span in minutes
 TIME_SPAN = 60
-
-# recursive computation of solutions (2 numbers and 1 operator)
-def solutions2(a, b):
-	return [a + o + b for o in '+-*/']
-
-# recursive computation of solutions (3 numbers and 2 operators)
-def solutions3(a, b, c):
-	return [a + o1 + b + o2 + c for o1 in '+-*/' for o2 in '+-*/'] 
-
-# recursive computation of solutions (4 numbers, 2 operators and 1 equality sign)
-def solutions4(a, b, c, d):
-	l = [a + '==' + f for f in solutions3(b, c, d)]
-	l += [f1 + '==' + f2 for f1 in solutions2(a, b) for f2 in solutions2(c, d)]
-	return l + [f + '==' + d for f in solutions3(a, b, c)]
 
 # customized variant of eval() mapping an arithmetic-error to False
 def evalx(f):
@@ -30,7 +16,10 @@ def evalx(f):
 
 # ----------------------------- main program ---------------------------- #
 
-# compute minutes of the day as variable for the iterations
+# list of operators that can be inserted and must be tested
+OPS = ['+', '-', '*', '/', '==']
+
+# compute minutes of the day as loop variable
 minutesOfDay = int(START_TIME[:2]) * 60 + int(START_TIME[3:])
 
 # count the minutes given by the time span
@@ -51,8 +40,12 @@ for i in range (TIME_SPAN):
 	else: 
 		c, d = str(minutes)[0], str(minutes)[1]
 
-	# compute and print solutions
-	print (list(filter(lambda x : evalx(x), solutions4(a, b, c, d))))
+	# compute solutions
+	l = [a + o1 + b + o2 + c + o3 + d for o1 in OPS for o2 in OPS for o3 in OPS]
+	l = list(filter(lambda x : evalx(x) and x.count('=') == 2, l))
+
+	# print solutions
+	print (l)
 
 	# icrement minutes of the day
 	minutesOfDay = (minutesOfDay + 1) % (24 * 60)
