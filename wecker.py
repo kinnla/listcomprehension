7ad10ea76a7ca32018aacac8220ed9ac1c8e54f6
Till Zoppke <till.zoppke@fu-berlin.de>
2017-02-26 10:29:06 +0100

    refactoring, removing sign variations

--- wecker.py
+++ wecker.py
@@ -1,39 +1,24 @@
-
-
-operators = ['+', '-', '*', '/']
-signs = ['', '-']
+"""Computes possible equations for the wecker problem"""
 
 def evalx(f):
-	try:
-		return eval(f)
-	except:
-		return False
-
-def gen_formulas1(a):
-	return [str(a), '-' + str(a)]
+	try: return eval(f)
+	except: return False
 
 def gen_formulas2(a, b):
-	global operators, signs
-	return [s1 + str(a) + o + s2 + str(b) for s1 in signs for s2 in signs for o in operators]
+	return [a + o + b for o in '+-*/']
 
 def gen_formulas3(a, b, c):
-	global operators, signs
-	l1 = [s1 + '(' + f1 + ')' + o + f2 for s1 in signs for f1 in gen_formulas2(a, b) for o in operators for f2 in gen_formulas1(c)]
-	l2 = [f1 + o + s2 + '(' + f2 + ')' for s2 in signs for f1 in gen_formulas1(a) for o in operators for f2 in gen_formulas2(b, c)]
+	l1 = ['(' + f1 + ')' + o + c for f1 in gen_formulas2(a, b) for o in '+-*/']
+	l2 = [a + o + '(' + f2 + ')' for o in '+-*/' for f2 in gen_formulas2(b, c)]
 	return l1 + l2
 
 def check_eval(l1, l2):
-	"""Returns a valid formula or False"""
-	ll1 = [evalx(f) for f in l1]
-	ll2 = [evalx(f) for f in l2]
-	for i1 in range(len(l1)):
-		for i2 in range(len(l2)):
-			if ll1[i1] == ll2[i2]:
-				return l1[i1] + '=' + l2[i2]
-	return False
+	l = [f1 + '==' + f2 for f1 in l1 for f2 in l2]
+	return filter(lambda x : evalx(x), l)		
 
-
-times = [(2.0,1.0,float(a),float(b)) for a in range(6) for b in range(10)]
+times = [('2.', '1.', str(a)+'.', str(b)+'.') for a in range(6) for b in range(10)]
 for t in times:
-	print check_eval(gen_formulas1(t[0]) , gen_formulas3(t[1], t[2], t[3])) or check_eval(gen_formulas2(t[0], t[1]) , gen_formulas2(t[2], t[3])) or check_eval(gen_formulas3(t[0], t[1], t[2]) , gen_formulas1(t[3]))
-
+	l = check_eval([t[0]] , gen_formulas3(t[1], t[2], t[3]))
+	l += check_eval(gen_formulas2(t[0], t[1]) , gen_formulas2(t[2], t[3]))
+	l += check_eval(gen_formulas3(t[0], t[1], t[2]) , [t[3]])
+	print l
