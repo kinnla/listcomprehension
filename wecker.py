7948057c4aefc0c2c00986f0249cd507f531ce4c
Till Zoppke <till.zoppke@fu-berlin.de>
2017-02-26 10:46:24 +0100

    remove brackets and refactoring

--- wecker.py
+++ wecker.py
@@ -8,17 +8,16 @@ def gen_formulas2(a, b):
 	return [a + o + b for o in '+-*/']
 
 def gen_formulas3(a, b, c):
-	l1 = ['(' + f1 + ')' + o + c for f1 in gen_formulas2(a, b) for o in '+-*/']
-	l2 = [a + o + '(' + f2 + ')' for o in '+-*/' for f2 in gen_formulas2(b, c)]
-	return l1 + l2
+	return [a + o1 + b + o2 + c for o1 in '+-*/' for o2 in '+-*/'] 
 
-def check_eval(l1, l2):
-	l = [f1 + '==' + f2 for f1 in l1 for f2 in l2]
-	return filter(lambda x : evalx(x), l)		
+def gen_formulas4(a, b, c, d):
+	l = [a + '==' + f for f in gen_formulas3(b, c, d)]
+	l += [f1 + '==' + f2 for f1 in gen_formulas2(a, b) for f2 in gen_formulas2(c, d)]
+	return l + [f + '==' + d for f in gen_formulas3(a, b, c)]
+
 
 times = [('2.', '1.', str(a)+'.', str(b)+'.') for a in range(6) for b in range(10)]
 for t in times:
-	l = check_eval([t[0]] , gen_formulas3(t[1], t[2], t[3]))
-	l += check_eval(gen_formulas2(t[0], t[1]) , gen_formulas2(t[2], t[3]))
-	l += check_eval(gen_formulas3(t[0], t[1], t[2]) , [t[3]])
-	print l
+	l = gen_formulas4(t[0], t[1], t[2], t[3])
+	print (filter(lambda x : evalx(x), l))
+
