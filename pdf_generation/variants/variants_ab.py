f82f9d6e8bacdbc1c3bbc8fca820f9f846cb2987
Till Zoppke <till.zoppke@fu-berlin.de>
2017-05-20 10:33:45 +0200

    removing args as a parameter for variants

--- pdf_generation/variants/variants_ab.py
+++ pdf_generation/variants/variants_ab.py
@@ -47,11 +47,10 @@ def parse_args():
 	return parser.parse_args()
 
 
-def variants(tex_doc, args):
+def variants(tex_doc):
 	""" Generates variants of a tex documents
 
 	tex_doc -- the tex document as a string
-	args -- the command line options as a Namespace object
 	"""
 	
 	yield re.sub(r"\*A\*(.*?)\*\*\*.*?\*B\*", r'\1', tex_doc)
@@ -67,7 +66,7 @@ def main():
 		tex = ''.join(file.readlines())
 
 	# create PDF series
-	util.create_pdf_series(tex, args, variants(tex, args))
+	util.create_pdf_series(tex, args, variants(tex))
 
 	# open the combined pdf containing all variants
 	os.system('open ' + args.output)
