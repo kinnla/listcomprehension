2f2009f4d7578b14abf9be8c347612f3590d943e
Till Zoppke <till.zoppke@fu-berlin.de>
2017-05-20 12:36:03 +0200

    include consistency checks

--- pdf_generation/variants/variants_ab.py
+++ pdf_generation/variants/variants_ab.py
@@ -12,10 +12,7 @@ How to use:
   - example: *A*it's night time in Berlin***it's day time in Berlin*B*
 
 2. Run the script
-  - python3 variants_ab 
-
-example:
->> python3 variants_ab test_ab.tex
+  - python3 variants_ab variants_ab test_ab.tex
 
 Prerequisits:
 - latex
@@ -52,12 +49,41 @@ def variants(tex_doc):
 	tex_doc -- the tex document as a string
 	"""
 	
-	yield re.sub(r"\*A\*(.*?)\*\*\*.*?\*B\*", r'\1', tex_doc)
-	yield re.sub(r"\*A\*.*?\*\*\*(.*?)\*B\*", r'\1', tex_doc)
+	# for any of the three markers, create an iterator of its matches
+	iter1 = re.finditer(r"\*A\*", tex_doc)
+	iter2 = re.finditer(r"\*\*\*", tex_doc)
+	iter3 = re.finditer(r"\*B\*", tex_doc)
+
+	# now parallely iterate on all matches 
+	index_of_last_match = -1
+	while True:
+		matches = (next(iter1, None), next(iter2, None), next(iter3, None))
+
+		# if all iterators exhausted simultaneously: we are fine
+		if matches == (None,)*3:
+			break
+
+		# if any iterator exhausted before the others: that's a problem
+		assert not None in matches, "Inconsistent number of markers. Please check tex source."
+
+		# check that indices are ascending
+		assert index_of_last_match < matches[0].start(0), "New Variant started before old one ended"
+		assert matches[0].start(0) < matches[1].start(0), "Variant stated with *** instead of *A*"
+		assert matches[1].start(0) < matches[2].start(0), "Variant ended before ***"
+
+	l= re.findall(r"\*\*\*", tex_doc)
+	l=re.search(r"\*\*\*", tex_doc)
+	l=[r for r in re.finditer(r"\*\*\*", tex_doc)]
+	print(l)
+
+	regex_a = re.compile(r"\*A\*(.*?)\*\*\*.*?\*B\*", re.DOTALL)
+	regex_b = re.compile(r"\*A\*.*?\*\*\*(.*?)\*B\*", re.DOTALL)
+	yield re.sub(regex_a, r'\1', tex_doc)
+	yield re.sub(regex_b, r'\1', tex_doc)
 
 def main():
 
-	# parse command line arguments and eventually include output path
+	# parse command line arguments 
 	args = parse_args()
 	if not args.output:
 		args.output = args.texfile.replace('.tex','.pdf')
